bbf100fda1c408853cf8d8fc73b09c20e083122b
Zarah Zmith <sarah@zarahzmith.com>
2017-11-01 21:27:24 -0400

    Simple function with python

---
+++ greeting.py
@@ -0,0 +1,8 @@
+def print_greeting(name):
+    greeting = 'Hello, ' +name+ '!'
+    print(greeting)
+
+retrieved_name = input('Please enter your name ')
+#This above line appears in the terminal. Enter name in terminal to complete the script.
+print_greeting(retrieved_name)
+print('Script complete')
